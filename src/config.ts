535c84574d564bac92febd035da0188261fda330
GTNNN <48439624+GTNNN@users.noreply.github.com>
2023-11-09 23:01:28 +0800

    first commit

---
+++ src/config.ts
@@ -0,0 +1,14 @@
+export const SITE = {
+  title: 'tiann`s funny days',
+  author: 'tiann',
+  description: '记录程序员tiann的不枯燥生活，每周一发布，敬请期待',
+  keywords: 'tn96,Weekly,Blog,前端,macOS,Pack,Swift',
+  icon: '/assets/coffee-2-icon.png',
+  pic: 'https://cdn.fliggy.com/upic/2XFNGI.png',
+  homePage: 'https://tn96.fun',
+  wolai: 'https://www.wolai.com/gaodian/2m4d5D14wAWih5Mak82LJs',
+  // twitterId: 'HiTw93',
+  // githubId: 'tw93',
+  repo: 'GTNNN/funnydays'
+};
+
